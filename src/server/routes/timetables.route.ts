1c89771bbad1199d9d0a749da0e2273468d874dd
Luna Simons <luna@bddvlpr.com>
2022-10-02 23:14:18 +0200

    feat: 🎸 lesson merging
    From my observation (and school), classes are grouped by the lsNumber
    parameter and thus should be merged if the end / start time overlap.
    
    ✅ Closes: #4

--- src/server/routes/timetables.route.ts
+++ src/server/routes/timetables.route.ts
@@ -75,6 +75,23 @@ router.get(
 );
 
 const createCalendar = async (timetable: Lesson[], options: FormatOptions) => {
+  // TODO: This is a very inefficient way of merging lessons. It should be done in the retriever if possible.
+  timetable.forEach((lesson) => {
+    timetable
+      .filter(
+        (nextLesson) =>
+          nextLesson.date === lesson.date &&
+          nextLesson.lsnumber === lesson.lsnumber
+      )
+      .forEach((nextLesson) => {
+        if (nextLesson.startTime === lesson.endTime) {
+          logger.info(`Merging ${lesson.id} with ${nextLesson.id}.`);
+          lesson.endTime = nextLesson.endTime;
+          timetable.splice(timetable.indexOf(nextLesson), 1);
+        }
+      });
+  });
+
   return createEvents([
     ...timetable
       .filter((lesson) => {
