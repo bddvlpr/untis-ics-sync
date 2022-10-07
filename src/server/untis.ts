616b6bf683c7db06b759dcc026beec664c988671
Brendon Ignath <brendon.ignath@gmail.com>
2022-10-07 10:37:50 +0200

    Linted the changes

--- src/server/untis.ts
+++ src/server/untis.ts
@@ -4,16 +4,20 @@ import WebUntis from "webuntis";
 const createUntis = () => {
   dotenv.config();
 
-  var untis;
+  let untis;
 
   if (process.env.UNTIS_USERNAME && process.env.UNTIS_PASSWORD) {
     untis = new WebUntis(
       process.env.UNTIS_SCHOOL || "some school",
-      process.env.UNTIS_USERNAME!,
-      process.env.UNTIS_PASSWORD!,
+      process.env.UNTIS_USERNAME || "username",
+      process.env.UNTIS_PASSWORD || "password",
       process.env.UNTIS_BASEURL || "x.webuntis.com"
     );
-  } else untis = new WebUntis.WebUntisAnonymousAuth(process.env.UNTIS_SCHOOL || "some school", process.env.UNTIS_BASEURL || "x.webuntis.com");
+  } else
+    untis = new WebUntis.WebUntisAnonymousAuth(
+      process.env.UNTIS_SCHOOL || "some school",
+      process.env.UNTIS_BASEURL || "x.webuntis.com"
+    );
 
   untis.login();
   return untis;
