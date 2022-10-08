e92773a194ee1ef9f63365ae6edafdde223e6d88
Brendon Ignath <brendon.ignath@gmail.com>
2022-10-09 00:40:23 +0200

    feat: 🎸 anonymous login (by @freebreix)
    * Added anonym auth access when credentials missing
    
    * Linted the changes

--- src/server/untis.ts
+++ src/server/untis.ts
@@ -4,12 +4,21 @@ import WebUntis from "webuntis";
 const createUntis = () => {
   dotenv.config();
 
-  const untis = new WebUntis(
-    process.env.UNTIS_SCHOOL || "some school",
-    process.env.UNTIS_USERNAME || "some username",
-    process.env.UNTIS_PASSWORD || "some password",
-    process.env.UNTIS_BASEURL || "x.webuntis.com"
-  );
+  let untis;
+
+  if (process.env.UNTIS_USERNAME && process.env.UNTIS_PASSWORD) {
+    untis = new WebUntis(
+      process.env.UNTIS_SCHOOL || "some school",
+      process.env.UNTIS_USERNAME || "username",
+      process.env.UNTIS_PASSWORD || "password",
+      process.env.UNTIS_BASEURL || "x.webuntis.com"
+    );
+  } else
+    untis = new WebUntis.WebUntisAnonymousAuth(
+      process.env.UNTIS_SCHOOL || "some school",
+      process.env.UNTIS_BASEURL || "x.webuntis.com"
+    );
+
   untis.login();
   return untis;
 };
