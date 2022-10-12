d0618b20b200a9082d18184e10f9522023aba443
Luna Simons <luna@bddvlpr.com>
2022-10-12 21:11:31 +0200

    feat: 🎸 created redis healthcheck

--- src/server/routes/health.route.ts
+++ src/server/routes/health.route.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import redis from "../redis";
 
 const router = Router();
 
 router.use("/", async (req, res) => {
-  res.send("OK");
+  const warnings = [];
+
+  (!redis.isOpen || !redis.isReady) &&
+    warnings.push("Redis is not connected or in invalid state.");
+
+  res.status(warnings.length ? 500 : 200).json(warnings);
 });
 
 export default router;
