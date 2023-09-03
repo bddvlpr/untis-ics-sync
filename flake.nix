2bfb25abf2d3ec270343111c58e3e3941fbfb2f5
Luna Simons <luna@bddvlpr.com>
2023-09-03 12:20:23 +0200

    chore: reinitialized project for nest

--- flake.nix
+++ flake.nix
@@ -4,10 +4,9 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     ...
-  } @ inputs: let
+  }: let
     forAllSystems = nixpkgs.lib.genAttrs [
       "aarch64-linux"
       "aarch64-darwin"
@@ -15,18 +14,12 @@
       "x86_64-linux"
     ];
   in {
-    packages = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      default = import ./default.nix {inherit pkgs;};
-    });
-
     devShell = forAllSystems (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
       in
         pkgs.mkShell {
-          buildInputs = with pkgs; [nodejs yarn];
+          buildInputs = with pkgs; [nodejs yarn nest-cli];
         }
     );
   };
